0ee894edd96525c63e683ef5d284095b0e5e2292
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2021-10-21 16:42:52 -0700

    add title and description

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -19,7 +19,8 @@ const Layout = ({ children }) => {
       query SiteTitleQuery {
         site {
           siteMetadata {
-            title
+            defaultTitle
+            defaultDescription
           }
         }
       }
@@ -32,8 +33,8 @@ const Layout = ({ children }) => {
         Skip to the content
       </a>
       <Header
-        siteTitle={site.siteMetadata.title}
-        siteDescription={site.siteMetadata.description}
+        siteTitle={site.siteMetadata.defaultTitle}
+        siteDescription={site.siteMetadata.defaultDescription}
       />
       <main id="primary">{children}</main>
       <Footer siteTitle={site.siteMetadata.title} />
