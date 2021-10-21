6208d5168c9c8937735cb2b503843dbfb6234227
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2021-10-21 16:53:45 -0700

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -37,7 +37,7 @@ const Layout = ({ children }) => {
         siteDescription={site.siteMetadata.defaultDescription}
       />
       <main id="primary">{children}</main>
-      <Footer siteTitle={site.siteMetadata.title} />
+      <Footer siteTitle={site.siteMetadata.defaultTitle} />
     </>
   )
 }
